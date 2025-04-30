ec9e3e384d0e001427a7cdcf5b2332772ec6f6af
smilingions <196618294+smilingions@users.noreply.github.com>
2025-04-30 15:57:01 +0100

    fix: remove unnecessary codes

--- apps/chrome-extension/src/popup/popup.tsx
+++ apps/chrome-extension/src/popup/popup.tsx
@@ -2,38 +2,9 @@ import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import ToggleSwitch from './components/ToggleSwitch';
 import Footer from './components/Footer';
-import {
-  createClient,
-  InternetIdentity,
-  Plug,
-  Nfid,
-  defaultProviders,
-} from '@oranjlabs/icp-wallet-adapter';
-
-export const host =
-  import.meta.env.VITE_DFX_NETWORK === 'ic'
-    ? 'https://icp0.io'
-    : 'http://127.0.0.1:4943';
-
-export const provider =
-  import.meta.env.VITE_DFX_NETWORK === 'ic'
-    ? 'https://identity.ic0.app'
-    : 'http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943';
 
 function Popup() {
   const [isEnabled, setIsEnabled] = useState<boolean>(false);
-
-  const client = createClient({
-    providers: defaultProviders({
-      host,
-      providerUrl: provider,
-      whitelist: [],
-      autoConnect: true,
-    }),
-    globalProviderConfig: {
-      host,
-    },
-  });
 
   // Load saved state on mount
   useEffect(() => {
@@ -51,9 +22,6 @@ function Popup() {
     const value = enabled ? 'on' : 'off';
     chrome.storage.local.set({ strke: value }, () => {
       console.log('Saved state:', value);
-      if (!enabled) {
-        client.disconnect();
-      }
 
       // Reload current active tab
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -78,10 +46,6 @@ function Popup() {
       <Footer />
     </div>
   );
-}
-
-function createICPConnectors(config: any) {
-  return [new InternetIdentity(config), new Plug(config), new Nfid(config)];
 }
 
 export default Popup;
